f1beeef7e61d2b1ecbb221c07d1ea55c1d7624e6
Una <trewbot@gmail.com>
2021-09-02 21:12:16 -0700

    feat(yargs): add cli options

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 /**
  * Kramdown Sort Numbered Footnotes
  * @author Una Ada <una@anarchy.website>
@@ -6,23 +8,47 @@
  */
 
 import { readFile, writeFile } from 'fs/promises';
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
 
-readFile('in.kd')
-  .then(d =>
-    writeFile(
-      'out.kd',
-      ((i, o) =>
-        [
-          ...[...d.toString().matchAll(i('(\\d+)'))].reduce(
-            (m, [, v]) => (m.has(v) ? m : m.set(v, m.size + 1)),
-            new Map()
-          ),
-        ]
-          .reduce((t, [k, v]) => t.replace(i(k), o('x' + v)), d.toString())
-          .replace(i('(x)(\\d+)'), o('$2')))(
-        n => new RegExp(`\\[\\^${n}\\]`, 'gm'),
-        n => `[^${n}]`
-      )
-    ).catch(e => console.error(e))
+const sort = ({ d, f }) =>
+  readFile(f)
+    .then(t =>
+      writeFile(
+        d || f,
+        ((i, o) =>
+          [
+            ...[...t.toString().matchAll(i('(\\d+)'))].reduce(
+              (m, [, v]) => (m.has(v) ? m : m.set(v, m.size + 1)),
+              new Map()
+            ),
+          ]
+            .reduce((p, [k, v]) => p.replace(i(k), o('x' + v)), t.toString())
+            .replace(i('(x)(\\d+)'), o('$2')))(
+          n => new RegExp(`\\[\\^${n}\\]`, 'gm'),
+          n => `[^${n}]`
+        )
+      ).catch(e => console.error(e))
+    )
+    .catch(e => console.error(e));
+
+yargs(hideBin(process.argv))
+  .usage('Usage: $0 <file> [options]')
+  .command(
+    '$0 <file>',
+    'Sort the footnotes in a kramdown file.',
+    yargs =>
+      yargs.positional('file', {
+        alias: 'f',
+        describe: 'File to sort footnotes in',
+        type: 'string',
+      }),
+    sort
   )
-  .catch(e => console.error(e));
+  .option('dest', {
+    alias: 'd',
+    default: null,
+    describe: 'Set the destination of the sorted file',
+    type: 'string',
+  })
+  .help().argv;
